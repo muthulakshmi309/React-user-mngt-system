ee6cdb41752199fe5d586ed90279c621d501ad8b
muthulakshmi.a <muthulakshmi.arumugam@zeuslearning.com>
2018-08-13 01:18:01 +0530

    Render bootstrap table and fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,83 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Table, TableProps } from 'react-bootstrap';
 
 class App extends Component {
-  render() {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      usersList: [],
+      error: null,
+      isLoaded: false
+    };
+
+  }
+  componentDidMount() {
+    fetch("https://api.myjson.com/bins/pkisp")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            usersList: result.users
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+  
+  render() {  
+    const usersList = this.state.usersList;
+    let key = {};  
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <header className="App-header">         
+          <h1 className="App-title">User Management System</h1>
+        </header>        
+
+        
+        <Table striped bordered condensed hover>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Date of birth</th>
+            <th>Age</th>
+            <th>Email</th>
+            <th>Mobile</th>
+            <th>Active</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+        {
+           usersList.map((item, index) => {
+           return (<tr> 
+            <td>{ item.first_name + " " + item.last_name } </td>
+            <td> {new Intl.DateTimeFormat('en-US', { 
+                year: 'numeric', 
+                month: 'numeric', 
+                day: '2-digit' 
+        }).format(item.dob)}</td>
+            <td>{ item.first_name } </td>
+            <td>{ item.email } </td>
+            <td>{ item.phone } </td>
+            <td>{ item.active } </td>
+            <td>{  } </td>
+          </tr>);
+         })
+        }
+        </tbody>
+      </Table>
       </div>
     );
   }
